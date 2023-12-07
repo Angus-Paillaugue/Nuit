694b3108c072b70d05d8b964bc72017ecf098baa
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2023-12-07 21:13:42 +0100

    Navbar

--- frontend/nav/nav.js
+++ frontend/nav/nav.js
@@ -3,15 +3,16 @@ $(document).ready(function(){
   $("head").append('<script src="https://cdn.tailwindcss.com"></script>');
   
   // Add navbar to the page
-  $('nav').load('https://raw.githubusercontent.com/AldanariW/Nuit/main/frontend/src/navbar.html');
-  
-  // Nav dropdown
-  $("#hamburgerButton").click(() => {
-    $("#dropdown").toggleClass("opacity-0 opacity-100 z-50, -z-10");
+  $('nav').load('https://raw.githubusercontent.com/AldanariW/Nuit/main/frontend/src/navbar.html', () => {
+    // Nav dropdown
+    $("#hamburgerButton").click(() => {
+      $("#dropdown").toggleClass("opacity-0 opacity-100 z-50, -z-10");
+    });
+    
+    // Update score in navbar
+    updateScoreInNavBar();
   });
   
-  // Update score in navbar
-  updateScoreInNavBar();
 });
 
 const updateScoreInNavBar = () => {
