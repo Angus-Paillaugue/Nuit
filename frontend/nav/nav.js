a56f6c06da3d2fd8d78a95db4dd2f1d59839ab2a
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2023-12-07 21:17:57 +0100

    Changed nav absolute url

--- frontend/nav/nav.js
+++ frontend/nav/nav.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $("head").append('<script src="https://cdn.tailwindcss.com"></script>');
   
   // Add navbar to the page
-  $('nav').load('https://raw.githubusercontent.com/AldanariW/Nuit/main/frontend/src/navbar.html', () => {
+  $('nav').load('https://raw.githubusercontent.com/AldanariW/Nuit/main/frontend/nav/navbar.html', () => {
     // Nav dropdown
     $("#hamburgerButton").click(() => {
       $("#dropdown").toggleClass("opacity-0 opacity-100 z-50, -z-10");
