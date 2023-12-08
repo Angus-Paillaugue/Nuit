6f3997fed54655c2a8639f67fe193241bf106691
CRR4715A <rafael.curylo@etu.iut-tlse3.fr>
2023-12-08 02:29:54 +0100

    - Added card management in Swipe

--- frontend/Swipe/main.js
+++ frontend/Swipe/main.js
@@ -1,31 +1,84 @@
 // Make the DIV element draggable:
 
+function getCurrentCard() {
+    console.log(Array.from(document.getElementsByClassName("card")).map(e => (parseInt(e.id))));
+    return Array.from(document.getElementsByClassName("card")).map(e => (parseInt(e.id)))[0];
+}
+
 function onYesClick() {
-    console.log("Clicked on Yes");
-    swipeRigth;
-    moveForward;
-    dropCard;
+    console.log("Clicked on Yes" + getCurrentCard());
+    swipeRigth();
+
+    moveForward();
+
 }
 
 function onNoClick() {
-    console.log("Clicked on No");
-    swipeLeft;
-    moveForward;
-    dropCard;
+    console.log("Clicked on No" + getCurrentCard());
+    swipeLeft();
+
+    moveForward();
+
 }
 
 function moveForward() {
 
+    Array.from(document.getElementsByClassName("card")).forEach(e => {
+        console.log(e);
+        e.style.opacity = Number(e.style.opacity) + 0.2;
+        e.style.top = (Number(e.style.top.slice(0, -2)) + 20) + "px";
+    });
+
 }
 
-function dropCard() {
-
+function dropCurrentCard() {
+    document.getElementById(getCurrentCard()).remove();
 }
 
 function swipeRigth() {
-
+    let currentCard = document.getElementById(getCurrentCard());
+    if (currentCard != null) {
+        
+        let pos = 0;
+        let opacity = 1;
+        const threshold = 200;
+        let id = null;
+        clearInterval(id);
+        id = setInterval(frame, 1000 / (threshold));
+        function frame() {
+            if (pos > threshold) {
+                clearInterval(id);
+                dropCurrentCard();
+            } else {
+                pos += (pos + 10) / 18;
+                opacity = 1 - ((pos / threshold) * 10);
+                currentCard.style.left = pos + "px";
+                currentCard.style.opacity = opacity;
+            }
+        }
+    }
 }
 
 function swipeLeft() {
+    let currentCard = document.getElementById(getCurrentCard());
+    if (currentCard != null) {
 
+        let pos = 0;
+        let opacity = 1;
+        const threshold = 200;
+        let id = null;
+        clearInterval(id);
+        id = setInterval(frame, 1000 / (threshold));
+        function frame() {
+            if (pos < -threshold) {
+                clearInterval(id);
+                dropCurrentCard();
+            } else {
+                pos -= (-pos + 10) / 18;
+                opacity = 1 - (((-pos) / threshold) * 10);
+                currentCard.style.left = pos + "px";
+                currentCard.style.opacity = opacity;
+            }
+        }
+    }
 }
