1767164c75783eb840b6a97e527ab3f5ab5e78d3
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2023-12-08 05:34:50 +0100

    Path fixes 2

--- frontend/nav/nav.js
+++ frontend/nav/nav.js
@@ -38,7 +38,7 @@ const navbarMarkup = `
 `;
 
 $(document).ready(function() {
-    const baseURl = location.origin + "/nav";
+    const baseURl = location.origin;
     // Add Tailwind CSS
     $("head").append(`
     <link href="${baseURl}/nav/navbar.css" rel="stylesheet">`);
